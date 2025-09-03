fbf9a831e5e74e95fbc9cdd12f6c9206994ddc07
Minevitably <plotmax@163.com>
2025-09-03 22:06:40 +0800

    q0292_nim_game fun

---
+++ q0292_nim_game/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <string>
+#include <map>
+
+using namespace std;
+
+class Solution {
+public:
+    bool canWinNim(int n) {
+        return n % 4;
+    }
+};
+
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
