5d9fe8b857458f37cc79e7d3eac1749b506f142e
Minevitably <plotmax@163.com>
2025-09-06 21:35:46 +0800

    q0344_reverse_string trivial

---
+++ q0344_reverse_string/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <string>
+#include <map>
+
+using namespace std;
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int left = 0;
+        int right = s.size() - 1;
+        while (left < right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+};
+
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
