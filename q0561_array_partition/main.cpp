91297015353fb27fcff234ecdb70f84a2f6dd552
Minevitably <plotmax@163.com>
2025-09-28 20:13:39 +0800

    q0561_array_partition trivial

---
+++ q0561_array_partition/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <string>
+#include <map>
+#include <set>
+
+using namespace std;
+
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+        std::sort(nums.begin(), nums.end());
+        int sum = 0;
+        for (int i = 0; i < nums.size(); i += 2) {
+            sum += nums[i];
+        }
+        return sum;
+    }
+};
+
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
